907c04d650b42d20cef795963d0f4bff2bba1d9b
Teodora Rujoiu <rujoiu.teodoramaria@gmail.com>
2023-07-26 18:39:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 // cors
 app.use(cors({
-  origin: 'https://chipper-snickerdoodle-801550.netlify.app',
+  origin: 'https://bucolic-lily-0155a3.netlify.app',
   credentials: true
 }))
 
